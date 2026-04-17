f811eecad97905270163473bdf9a8ef6f542a2f7
Amirhesp <amir.h.esmailpor@gmail.com>
2026-04-17 18:01:58 +0330

    Build: client v0.0.24

--- libraries/client/build.gradle.kts
+++ libraries/client/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 mavenPublishing {
-  coordinates("org.holochain.androidserviceruntime", "client", "0.0.23")
+  coordinates("org.holochain.androidserviceruntime", "client", "0.0.24")
 
   pom {
     name.set("Holochain Service Client")
